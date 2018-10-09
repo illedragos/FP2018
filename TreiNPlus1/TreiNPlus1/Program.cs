bd87cdbad8dfe4b8cff5a993605831ae6ce20195
Horea Oros <horea.oros@gmail.com>
2018-10-09 12:38:29 +0300

    Adaugat cod pentru conjectura 3n+1

--- TreiNPlus1/TreiNPlus1/Program.cs
+++ TreiNPlus1/TreiNPlus1/Program.cs
@@ -10,6 +10,29 @@ namespace TreiNPlus1
     {
         static void Main(string[] args)
         {
+            // variabila = nume pentru o locatie de memorie
+            int a;
+
+
+            string line;
+
+            Console.WriteLine("Introduceti un numar: ");
+            line = Console.ReadLine();
+            a = int.Parse(line);
+
+
+            while (a != 1)
+            {
+                if(a % 2 == 1)
+                    a = 3 * a + 1;
+                else
+                    a = a / 2;
+                Console.Write("{0} ", a);
+            }
+
+            Console.WriteLine();
+
+
         }
     }
 }
