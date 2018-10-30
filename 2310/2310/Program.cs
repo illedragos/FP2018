252f83c92e7c0c14a261b35acb7738d2d3a9581a
Horea Oros <horea.oros@gmail.com>
2018-10-30 11:55:46 +0200

    add new project

---
+++ 2310/2310/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *******************
+            // nu toate numerele reale sunt reprezenabile exact pe float / double
+            double eps = 0.001;
+
+            double suma = 0.0;
+
+            for (int i = 1; i <= 1000000; i++)
+            {
+                suma = suma + eps;
+            }
+            Console.WriteLine(suma);
+
+            // ****************
+
+
+            // calculam numarul de zile dintre 2 date calendaristice
+
+            int z1 = 23, l1 = 10, a1 = 2018;
+            int z2 = 14, l2 = 9, a2 = 1999;
+
+
+            int diff = 0;
+
+
+
+            // calcul...
+
+
+            while (z1 != z2 || l1 != l2 || a1 != a2)
+            {
+                diff++;
+                // schimb prima data calend cu data zilei anterioare
+                if (z1 > 1)
+                {
+                    z1--;
+                }
+                else
+                {
+                    switch (l1)
+                    {
+                        case 1:
+                            z1 = 31;
+                            l1 = 12;
+                            a1--;
+                            break;
+                        case 2:
+                        case 4:
+                        case 6:
+                        case 8:
+                        case 9:
+                        case 11:
+                            z1 = 31;
+                            l1--;
+                            break;
+                        case 5:
+                        case 7:
+                        case 10:
+                        case 12:
+                            z1 = 30;
+                            l1--;
+                            break;
+                        case 3:
+                            if ((a1 % 4 == 0 && a1 % 100 != 0) || (a1 % 400 == 0))
+                                z1 = 29;
+                            else
+                                z1 = 28;
+                            l1 = 2;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+
+
+
+            Console.WriteLine("Numarul de zile dintre cele doua date calendaristice este: {0}", diff);
+
+
+            DateTime d1 = new DateTime(2018, 10, 23);
+            DateTime d2 = new DateTime(1999, 9, 14);
+            TimeSpan difts = d1.Subtract(d2);
+            Console.WriteLine(difts.TotalDays);
+
+
+
+            // random
+            Random rnd = new Random();
+            int t0 = 0, t1 = 1;
+            int v;
+            for (int i = 0; i < 100; i++)
+            {
+                v = rnd.Next(2);
+                if (v == 0)
+                {
+                    t0++;
+                }
+                else
+                {
+                    t1++;
+                }
+            }
+
+            Console.WriteLine("t0 = {0}, t1 = {1}", t0, t1);
+
+        }
+    }
+}
---
+++ 3010/3010/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace _3010
+{
+    class Program
+    {
+        #region Game
+        static void Game()
+        {
+            int stake = 50;
+            int goal = 100;
+            int times = 10000;
+            int bets = 0;
+            int sum = 0;
+            Random rnd = new Random();
+
+            int wins = 0, looses = 0;
+            for (int i = 0; i < times; i++)
+            {
+                bets = 0;
+                stake = 50;
+                while (stake < goal && stake > 0)
+                {
+                    if (rnd.Next(2) == 0)
+                    {
+                        stake--;
+                    }
+                    else
+                    {
+                        stake++;
+                    }
+                    bets++;
+                }
+                sum += bets;
+                if (stake == 0)
+                {
+                    looses++;
+                }
+                else
+                {
+                    wins++;
+                }
+            }
+            Console.WriteLine("Wins: {0}, Looses: {1}, Average Bets: {2}", wins, looses, sum / times);
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            // Determinarea numarului de divizori ai unui numar natural
+            // Determinarea sumei divizorilor unui numar natural;
+
+            ulong n;
+
+            string line;
+            line = Console.ReadLine();
+
+            n = uint.Parse(line);
+
+            //Console.WriteLine("Numarul de divizori este: {0}", NumarDivizori2(n));
+            //Console.WriteLine("Suma divizorilor este: {0}", SumaDivizori(n));
+
+            Console.WriteLine("Numarul de divizori este: {0}", NumarDivizoriFast(n));
+            //Console.WriteLine("Suma divizorilor este: {0}", SumaDivizoriFast(n));
+        }
+
+        private static ulong NumarDivizoriFast(ulong n)
+        {
+            ulong p = 1;
+            ulong d = 2;
+            ulong a = 0;
+            while (n > 1)
+            {
+                a = 0;
+                while (n % d == 0)
+                {
+                    a++;
+                    n = n / d;
+                }
+                p = p * (a + 1);
+                d++;
+            }
+            return p;
+        }
+
+        private static ulong SumaDivizori(ulong n)
+        {
+            ulong suma = 0;
+
+            suma = n;
+            for (ulong d = 1; d <= n / 2; d++)
+            {
+                if (n % d == 0)
+                {
+                    suma += d;
+                }
+            }
+            return suma;
+        }
+
+        private static ulong NumarDivizori(ulong n)
+        {
+            ulong contor = 1;
+            if (n == 1)
+            {
+                return 1;
+            }
+            for (ulong d = 1; d <= n / 2; d++)
+            {
+                if (n % d == 0)
+                {
+                    contor++;
+                }
+            }
+            return contor;
+        }
+        private static ulong NumarDivizori2(ulong n)
+        {
+            ulong contor = 2;
+
+            if (n == 1)
+            {
+                return 1;
+            }
+            ulong d;
+            for (d = 2;  d * d < n; d++)
+            {
+                if (n % d == 0)
+                {
+                    contor += 2;
+                }
+            }
+            if (d * d == n )
+            {
+                contor++;
+            }
+            return contor;
+        }
+    }
+}
